79efe9aea6feeafca8435b92afe774192b8513a8
Danny Luu <cluu79@gmail.com>
2019-11-13 09:04:12 -0600

    set up for homepage and also installed SASS because regular CSS is no good

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+
 import './App.css';
+import Homepage from './homepage.component';
 
 function App() {
   return (
     <div>
-      hello world
+     <Homepage />
       
     </div>
   );
---
+++ src/homepage.component.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Laptops</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Desktops</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Consoles</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hardwares</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Appliances</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">BTBB-Toys</h1>
+          <span className="subtitle">BUY NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
