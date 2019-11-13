942b71b9958eff05d2eace05d1aadc3a16e4c8c4
Danny Luu <cluu79@gmail.com>
2019-11-13 10:08:56 -0600

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
-
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
+import { Switch, Route } from 'react-router-dom'
+
+const LaptopPage = () => (
+  <div>
+    <h1>Laptop Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-     <Homepage />
-      
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/Laptops" component={LaptopPage} />
+      </Switch>
     </div>
   );
 }
